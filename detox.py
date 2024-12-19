3cb26333448976dbdb4ce6f3101339638454ccc3
Anastasia <91737702+ayagnych@users.noreply.github.com>
2024-12-19 12:15:14 +0300

    Add files via upload

--- detox.py
+++ detox.py
@@ -38,43 +38,35 @@ class DeToxEdit():
             self.edit_layer_range = edit_layer_range
 
 
-    def _load_preference_data(self):
+    def _load_preference_data(self, data_path):
+        """
+        Загружает preference data из указанного файла.
+        Args:
+            data_path (str): Путь к файлу с preference данными для выбранного домена.
+        """
         num_dps = self.pref_data_dps
-        filedir = os.path.join(os.environ["DATASET_DIR"], 'toxicity_pairwise')
-        filepath = os.path.join(filedir, 'split_0.jsonl')
-
-        if not os.path.exists(filepath):
-            logging.info('Preference data not found. Downloading...')
-            os.makedirs(filedir, exist_ok=True)
-            url = 'https://drive.google.com/file/d/1BmBkhNS4R2z5UwqVu5GhaTvFFeWOJfPC/view?usp=drive_link'
-            os.system(f'gdown --id {url.split("/")[-2]} -O {filedir}/toxicity_pairwise.zip')  # Download file from google drive
-            os.system(f'unzip {filedir}/toxicity_pairwise.zip -d {filedir}')  # Unzip the file
-            os.system(f'rm {filedir}/toxicity_pairwise.zip')  # Delete the zip file
-            os.system(f'mv {filedir}/toxicity_pairwise/* {filedir}')  # Move the files in the subdirectory to the parent directory
-            os.system(f'rm -r {filedir}/toxicity_pairwise')  # Delete the subdirectory
-            assert os.path.exists(filepath), 'Preference data download failed.'
-            logging.info('Done.')
 
         preferred_data, non_preferred_data = [], []
-        with open(filepath, 'r') as f:
+        with open(data_path, 'r') as f:
             for line in f:
                 preferred_data.append(json.loads(line)['unpert_gen_text'])
                 non_preferred_data.append(json.loads(line)['pert_gen_text'])
 
-        if num_dps != -1:  # 4096 points
+        if num_dps != -1:  # Ограничение количества точек
             if not self.random_dps:
                 preferred_data = preferred_data[:num_dps]
                 non_preferred_data = non_preferred_data[:num_dps]
             else:
-                indices = np.random.choice(len(preferred_data), num_dps, replace=False)
+                indices = np.random.choice(len(preferred_data), num_dps, replace=True)
                 preferred_data = [preferred_data[i] for i in indices]
                 non_preferred_data = [non_preferred_data[i] for i in indices]
         logging.info(f'Loaded {len(preferred_data)} preferred and {len(non_preferred_data)} non-preferred samples.')
 
-        preferred_inputs = self.tokenizer(preferred_data, return_tensors="pt", padding=True, truncation=True, max_length=128)  # 128, 37
-        non_preferred_inputs = self.tokenizer(non_preferred_data, return_tensors="pt", padding=True, truncation=True, max_length=128)  # 128, 37
+        preferred_inputs = self.tokenizer(preferred_data, return_tensors="pt", padding=True, truncation=True, max_length=128)
+        non_preferred_inputs = self.tokenizer(non_preferred_data, return_tensors="pt", padding=True, truncation=True, max_length=128)
 
         return preferred_inputs, non_preferred_inputs
+
 
 
     def _get_hidden_sentence_embeddings(self, inputs):
@@ -133,8 +125,8 @@ class DeToxEdit():
         return hidden_sent_embs
 
 
-    def _get_preference_matrix(self):
-        preferred_inputs, non_preferred_inputs = self._load_preference_data()
+    def _get_preference_matrix(self, data_path):
+        preferred_inputs, non_preferred_inputs = self._load_preference_data(data_path)
 
         preferred_sent_embs = self._get_hidden_sentence_embeddings(preferred_inputs)  # (L, N, D)
         non_preferred_sent_embs = self._get_hidden_sentence_embeddings(non_preferred_inputs)  # (L, N, D)
@@ -150,26 +142,31 @@ class DeToxEdit():
                 d = preference_matrix[layer_num].to(torch.float32)
                 pref = deepcopy(preferred_sent_embs[layer_num].to(torch.float32))
 
+                # Регуляризация Тихонова
+                lambda_reg = 1e-5  # Регуляризационный коэффициент
                 u, s, vt = torch.linalg.svd(pref, full_matrices=False)  # (N, D) -> (N, N), (N,), (N, D)
-                projection_vector = vt[0].unsqueeze(dim=-1)  # (D, 1)
-                P = projection_vector @ projection_vector.T  # (D, D)
-                I = torch.eye(projection_vector.shape[0]).to(pref.device)  # (D, D)
-                d = d @ (I - P)  # (N, D) @ (D, D) -> (N, D)
-                preference_matrix[layer_num] = d.to(preference_matrix[layer_num].dtype) # d
+
+                # Добавление Tikhonov regularization
+                s_reg = s / (s**2 + lambda_reg)  # Регуляризованные сингулярные значения
+                projection_matrix = (vt.T * s_reg) @ vt  # P = V Σ_reg V^T
+
+                d = d @ (torch.eye(projection_matrix.shape[0], device=d.device) - projection_matrix)  # (N, D) @ (D, D)
+                preference_matrix[layer_num] = d.to(preference_matrix[layer_num].dtype)
 
         return preference_matrix
 
 
-    def get_ats(self):
 
-        preference_matrix = self._get_preference_matrix()  # (L, N, D)
+    def get_ats(self, data_path):
+
+        preference_matrix = self._get_preference_matrix(data_path)  # (L, N, D)
         ats = {}
 
         for key in self.model.state_dict():
             if 'weight' in key and 'mlp' in key:
                 layer_num = int(key.split('.')[2])  # Format: transformer.h.19.mlp.c_fc.weight
                 ats[key] = preference_matrix[layer_num]
-        return ats
+        return ats, preference_matrix
 
 
     def svd_on_ats(self, ats):
@@ -185,7 +182,7 @@ class DeToxEdit():
             logging.debug(f'Calculating SVD for: {key}')
             M = ats[key].to(torch.float32)  # SVD function only works with float32
 
-            u, s, vt = torch.linalg.svd(M.cuda(), full_matrices=False)  # Skinny SVD, vt is V^T
+            u, s, vt = torch.linalg.svd(M, full_matrices=False)  # Skinny SVD, vt is V^T
             svd[key] = {'u': u.cpu(), 's': s.cpu(), 'v': vt.T.cpu()}
         logging.info('SVD of ATS calculated.')
         return svd
@@ -253,15 +250,15 @@ class DeToxEdit():
 
                     if self.model_category in ['llama', 'mistral', 'opt', 'gptj']:
                         modified_weight = modified_weight.T
-                    edited_state_dict[key] = modified_weight.to('cuda').contiguous()  # contiguous for saving to disk
+                    edited_state_dict[key] = modified_weight.to('cpu').contiguous()  # contiguous for saving to disk
 
         self.model.load_state_dict(edited_state_dict, assign=True)
         logging.info('Edited model created.')
         return self.model
 
 
-    def setup_for_edits(self):
-        ats = self.get_ats()
+    def setup_for_edits(self, data_path):
+        ats = self.get_ats(data_path)
         svd = self.svd_on_ats(ats)
         del ats
         self.toxic_subspace = self.find_p_toxic(svd)
@@ -269,20 +266,16 @@ class DeToxEdit():
         torch.cuda.empty_cache()
 
 
-    def apply_edit_end_to_end(self, edit_keys=True, edit_values=True, layer_range=None):
+    def apply_edit_end_to_end(self, data_path, edit_keys=True, edit_values=True, layer_range=None):
         # Measure speed and memory use
         import time
         import psutil
         import pynvml
         start_time = time.time()
         before_memory = psutil.virtual_memory().used
-        pynvml.nvmlInit()
-        handle = pynvml.nvmlDeviceGetHandleByIndex(0)
-        info = pynvml.nvmlDeviceGetMemoryInfo(handle)
-        before_gpu_memory_used = info.used
 
         # Find P_toxic
-        self.setup_for_edits()
+        self.setup_for_edits(data_path)
 
         # Apply edit
         edited_model = self.edit_model(self.toxic_subspace, edit_keys, edit_values, layer_range)
@@ -291,10 +284,9 @@ class DeToxEdit():
         end_time = time.time()
         time.sleep(1)
         after_memory = psutil.virtual_memory().used
-        info = pynvml.nvmlDeviceGetMemoryInfo(handle)
-        after_gpu_memory_used = info.used
-        print(f"Elapsed time: {end_time - start_time} seconds")
-        print(f"System Memory Used: {(after_memory - before_memory) / (1024 * 1024)} MB")
-        print(f"GPU Memory Used: {(after_gpu_memory_used - before_gpu_memory_used) / (1024 ** 2)} MB")
+        # info = pynvml.nvmlDeviceGetMemoryInfo(handle)
+        # after_gpu_memory_used = info.used
+        # print(f"Elapsed time: {end_time - start_time} seconds")
+        # print(f"System Memory Used: {(after_memory - before_memory) / (1024 * 1024)} MB")
 
         return edited_model
